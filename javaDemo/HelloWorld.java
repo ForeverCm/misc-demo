98ce229404cac39f3c8249befd08839e1ff55cd2
chenmeng08 <chenmeng08@baidu.com>
2023-04-28 15:28:28 +0800

    update

--- javaDemo/HelloWorld.java
+++ javaDemo/HelloWorld.java
@@ -1,20 +1,53 @@
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.StringUtils;
 import java.io.BufferedReader;    
 import java.io.IOException;    
 import java.io.InputStream;    
-import java.io.InputStreamReader;   
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;   
 
 public class HelloWorld
 { 
     public static void main(String[] args) throws IOException, InterruptedException {
-		String cmd = "/Users/chenmeng08/t3jar/mp4DecryptMac/t3javaVM/temurin-11.jdk/Contents/Home/bin/java -jar /Users/chenmeng08/t3jar/mp4DecryptMac/bars-sdk-0.3.2.jar -f /Users/chenmeng08/t3jar/157627879086026949_1676885572493.mp4 -i /3a20f734a8824d1b916808ac8be5c618/157627704088229700/157627879086026949/157627879086026949_1676885572493.mp4 -t 2426e6d67d238e10438904e624317202 -o out.mp4";
-        Process pid = Runtime.getRuntime().exec(cmd);
-        // 获取外部程序标准输出流
-        new Thread(new OutputHandlerRunnable(pid.getInputStream())).start();
-        // 获取外部程序标准错误流
-        new Thread(new OutputHandlerRunnable(pid.getErrorStream())).start();
-        int exitValue = pid.waitFor();
-        System.out.println("Process exitValue: " + exitValue);
+        System.out.println("Process exitValue: " + "aaaaaa");
+		// String cmd = "/Users/chenmeng08/t3jar/mp4DecryptMac/t3javaVM/temurin-11.jdk/Contents/Home/bin/java -jar /Users/chenmeng08/t3jar/mp4DecryptMac/bars-sdk-0.3.2.jar -f /Users/chenmeng08/t3jar/157627879086026949_1676885572493.mp4 -i /3a20f734a8824d1b916808ac8be5c618/157627704088229700/157627879086026949/157627879086026949_1676885572493.mp4 -t 2426e6d67d238e10438904e624317202 -o out.mp4";
+        // Process pid = Runtime.getRuntime().exec(cmd);
+        // // 获取外部程序标准输出流
+        // new Thread(new OutputHandlerRunnable(pid.getInputStream())).start();
+        // // 获取外部程序标准错误流
+        // new Thread(new OutputHandlerRunnable(pid.getErrorStream())).start();
+        // int exitValue = pid.waitFor();
+        // System.out.println("Process exitValue: " + exitValue);
     }
+
+
+    public static String decryptCTRMode(final String encryptedStr, String key, String ivKey) {
+        if (!StringUtils.isNotBlank(encryptedStr)) {
+            return null;
+        }
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
+        Cipher encipher = Cipher.getInstance("AES/CTR/pkcs5padding");
+        IvParameterSpec iv = new IvParameterSpec(ivKey.getBytes(StandardCharsets.UTF_8));
+        encipher.init(Cipher.DECRYPT_MODE, keySpec, iv);
+        // 鍏堢敤BASE64瑙ｅ瘑
+        byte[] encryptedBytes = Base64.decodeBase64(encryptedStr);
+        // 鐒跺悗鍐岮ES瑙ｅ瘑
+        byte[] originalBytes = encipher.doFinal(encryptedBytes);
+        // 杩斿洖瀛楃涓�
+        return new String(originalBytes, StandardCharsets.UTF_8);
+    }
+
+    private void decodeBase64(String encryptedStr) {
+        // TODO
+    }
+
+
+
 
     private static class OutputHandlerRunnable implements Runnable {
         private InputStream in;
--- php-demo/exec.php
+++ php-demo/exec.php
@@ -1,4 +1,54 @@
 <?php
-exec('/Users/chenmeng08/t3jar/mp4DecryptMac/t3javaVM/temurin-11.jdk/Contents/Home/bin/java -jar /Users/chenmeng08/t3jar/mp4DecryptMac/bars-sdk-0.3.2.jar -f /home/work/t3/t3jar/157627879086026949_1676885572493.mp4 -i /3a20f734a8824d1b916808ac8be5c618/157627704088229700/157627879086026949/157627879086026949_1676885572493.mp4 -t 2426e6d67d238e10438904e624317202 -o /home/work/t3/t3jar/cmcm.mp4 2>&1', $array);
-var_dump($array);
+// exec('/Users/chenmeng08/t3jar/mp4DecryptMac/t3javaVM/temurin-11.jdk/Contents/Home/bin/java -jar /Users/chenmeng08/t3jar/mp4DecryptMac/bars-sdk-0.3.2.jar -f /home/chenmeng08/t3/t3jar/157627879086026949_1676885572493.mp4 -i /3a20f734a8824d1b916808ac8be5c618/157627704088229700/157627879086026949/157627879086026949_1676885572493.mp4 -t 2426e6d67d238e10438904e624317202 -o /home/work/t3/t3jar/cmcm.mp4 2>&1', $array);
+// var_dump($array);
+
+
+
+
+class AesCTR
+{
+    public static function encrypt($str, $key, $iv)
+    {
+        return openssl_encrypt($str, 'AES-256-CTR', $key, OPENSSL_ZERO_PADDING, $iv);
+    }
+
+    public static function decrypt($encryptedStr, $key, $iv)
+    {
+        return openssl_decrypt($encryptedStr, 'AES-256-CTR', $key, OPENSSL_ZERO_PADDING, $iv);
+    }
+}
+
+
+
+// GYMOFemPkuv8zCNneHwOsTBzEbNDjizZ
+// Z6B3O2zFRN2TRcZMERGqxQLwv1FgziRn
+$plainText = 'long_long_ago';
+$exampleEncryptString = 'dauilOB05xd3DXzAwsRtKhMI4EQyCwTYjOpBqHsdcrYILrF9jqtZGTFPts9A0KymqR7u5v0o+rGtMTeq+qEztAZdu+aoTQiXMuJiz4+H6C4l7ZOiwfEsd0tlQYb8kC/gZhsNxmVRqaV9YFig/rXNYpFLO8X9xB9CTkjpLFuWSBlQi8m8';
+
+
+
+// 32 位
+$key = 'YlYOUjn1X3NRmj5BlY3NcYOAKscyKAJU';
+
+// 16 位
+$iv = 'BDAii1rTV1zivPjl';
+
+
+$aaa = '{"cuid":"951113EB266019542620","reqTime":1679539223,"callTime":1679539223,"seqNo":"4a90bd1910c87edd9038a03aafa0b7ce", "extends":"[]"}';
+echo AesCTR::encrypt($aaa, $key, $iv);
+
+echo "\r\n";
+
+echo AesCTR::decrypt($exampleEncryptString, $key, $iv);
+
+
+if (strpos('aaaaabcd','aaa') === 0) {
+    echo 'aaaa';
+}else {
+    echo 'bbbbb';
+}
+
+
+
+
 ?>
